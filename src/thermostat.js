3893732b9ef7b6c75c686a60209359b64829da17
minsi yang <yangminsi@gmail.com>
2020-06-09 16:42:45 +0100

    energyUsage function, passed test

--- src/thermostat.js
+++ src/thermostat.js
@@ -3,10 +3,12 @@
 class Thermostat {
   constructor () {
     this.MINIMUM_TEMPERATURE = 10;
-    this.temperature = 20;
+    this.DEFAULT_TEMPERATURE = 20
+    this.temperature = this.DEFAULT_TEMPERATURE;
     this.powerSavingMode = true;
     this.MAX_LIMIT_PSM_ON = 25;
     this.MAX_LIMIT_PSM_OFF = 32;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
   }
   getCurrentTemperature() {
     return this.temperature;
@@ -42,6 +44,20 @@ class Thermostat {
       return this.temperature === this.MAX_LIMIT_PSM_OFF;
     }
     return this.temperature === this.MAX_LIMIT_PSM_ON;
+  }
+
+  resetTemperature() {
+    this.temperature = this.DEFAULT_TEMPERATURE;
+  }
+
+  energyUsage() {
+    if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+      return 'low-usage';
+    }
+    if (this.temperature >= this.MEDIUM_ENERGY_USAGE_LIMIT && this.temperature <= this.MAX_LIMIT_PSM_ON) {
+      return 'medium-usage';
+    }
+    return 'high-usage';
   }
 
 }
