3d4881cb58cef54aa8619ca9c5dd6955ff6af061
victorvallet <victorvallet8@gmail.com>
2020-06-11 16:17:32 +0200

    Successfully get request and displays current city's weather

--- interface.js
+++ interface.js
@@ -1,41 +1,56 @@
-$(document).ready(function() {
+$(document).ready(function () {
   var thermostat = new Thermostat();
   updateTemperature();
 
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=1e90f3e390607c8ae6d86a559464894e', function(result) {
-  console.log(result.weather[0])
-});
 
-  $('#temperature-up').click(function() {
+
+  $('#get-weather').click(function () {
+    var currentCity = $("#current-city").val();
+    console.log(currentCity)
+    getWeather(currentCity)
+  });
+
+  function getWeather(city) {
+
+    $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1e90f3e390607c8ae6d86a559464894e`, function (result) {
+      var weather = result.weather[0].description;
+
+      $("#current-weather").html(weather);
+
+
+    });
+  }
+
+  $('#temperature-up').click(function () {
     thermostat.up();
     updateTemperature();
   });
 
-  $('#temperature-down').click(function() {
+  $('#temperature-down').click(function () {
     thermostat.down();
     updateTemperature();
   });
 
-  $('#temperature-reset').click(function() {
+  $('#temperature-reset').click(function () {
     thermostat.resetTemperature();
     updateTemperature();
   });
 
-  $('#powersaving-on').click(function() {
+  $('#powersaving-on').click(function () {
     thermostat.switchPowerSavingModeOn();
     $('#power-saving').text('on')
     updateTemperature();
   })
 
-  $('#powersaving-off').click(function() {
+  $('#powersaving-off').click(function () {
     thermostat.switchPowerSavingModeOff();
     $('#power-saving').text('off')
     updateTemperature();
   })
 
   function updateTemperature() {
-  $('#temperature').text(thermostat.temperature);
-  $('#temperature').attr('class', thermostat.energyUsage());
-}
+    $('#temperature').text(thermostat.temperature);
+    $('#temperature').attr('class', thermostat.energyUsage());
+  }
 
 });
